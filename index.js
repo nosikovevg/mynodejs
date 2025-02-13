0b959ee9a86ecb0998ce295f03355bd562e3bd62
Nos <zvezdazanatih@gmail.com>
2025-02-13 07:12:47 +0700

    n13022025

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const express = require('express');
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res)=> {
+    res.send(`<h1>hello from express</h1>`)
+});
+
+app.listen(PORT, ()=> {
+    console.log('App is listening on port ${PORT}');
+})
+
+
+
+
+
 console.log('Hello World');
